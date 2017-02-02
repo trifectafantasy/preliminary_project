82d25be6b38e9d7c2b554c8ead22b37c629cad9b
wesleychipeco <wescollege36@gmail.com>
2017-02-02 00:21:29 -0800

    Football and basketball trades

---
+++ app/football_trade_stats.js
@@ -0,0 +1,140 @@
+///// IMPORT JAVASCRIPT PACKAGES //////
+var express = require('express');
+var request = require('request');
+var cheerio = require('cheerio');
+var path = require('path');
+var pyshell = require('python-shell');
+var forEach = require('async-foreach').forEach;
+
+var mongo = require('mongodb');
+var assert = require('assert');
+// create callback function
+module.exports = function(req, res, db, sport, year, owner_number_list, callback) {
+
+	var complete_count = 0;
+	var football_owner_number_list = [];
+	//var owner_number_list = [1, 2];
+	//console.log(owner_number_list);
+
+	forEach(owner_number_list, function(owner_number ,index, array) {
+		db.collection('football_owners').find({"football_owner_number": parseInt(owner_number)}, {"owner_number": 1, "_id": 0}).toArray(function(e, docs) {
+			owner_number = docs[0]["owner_number"]
+			db.collection("owner" + owner_number + "_" + sport + "_stats_" + year).remove({});
+		})
+	})
+
+
+	forEach(owner_number_list, function(owner_number ,index, array) {
+
+		db.collection('football_owners').find({"football_owner_number": parseInt(owner_number)}, {"owner_number": 1, "_id": 0}).toArray(function(e, docs) {
+			//football_owner_number = owner_number_list[index];
+
+			owner_number = docs[0]["owner_number"];
+
+				football_owner_number = array[index];
+
+				var url = 'http://games.espn.com/ffl/activestats?leagueId=154802&seasonId=' + year + '&teamId=' + football_owner_number
+
+				// request for scrape
+				request(url, function(error, response, html) {
+
+
+					// if not an error
+					if(!error){
+
+						// use cheerio to traverse and scrape html 
+						var $ = cheerio.load(html);
+
+						scrape = $('tr.playerTableBgRowSubhead.tableSubHead');
+						//console.log(scrape.text());
+
+						rows = scrape.siblings();
+						//console.log(rows.text());
+
+						rows.each(function(j, element) {
+
+							// store scraped data for each team as json
+							var json = {
+								player: "",
+								PASS: "",
+								PASS_YDS: "",
+								PASS_TD: "",
+								PASS_INT: "",
+								RUSH: "",
+								RUSH_YDS: "",
+								RUSH_TD: "",
+								REC: "",
+								REC_YDS: "",
+								REC_TD: "",
+								REC_TAR: "",
+								MISC_FUML: "",
+								MISC_TD: "",
+								PTS: ""
+							}				
+
+							player_row = $(this).children();
+							//console.log(player_row.text());
+							player = player_row.first();
+							PASS = player.next().next();
+							PASS_YDS = PASS.next();
+							PASS_TD = PASS_YDS.next();
+							PASS_INT = PASS_TD.next();
+							RUSH = PASS_INT.next().next();
+							RUSH_YDS = RUSH.next();
+							RUSH_TD = RUSH_YDS.next();
+							REC = RUSH_TD.next().next();
+							REC_YDS = REC.next();
+							REC_TD = REC_YDS.next();
+							REC_TAR = REC_TD.next();
+							MISC_FUML = REC_TAR.next().next();
+							MISC_TD = MISC_FUML.next();
+							PTS = MISC_TD.next().next();
+
+			 				// store each scraped value in json as TEXT, INT, or FLOAT
+			 				player_name = player.text();
+			 				player_name = player_name.slice(0, player_name.indexOf(","));
+
+			 				json.player = player_name;
+			 				json.PASS = PASS.text();
+			 				json.PASS_YDS = parseInt(PASS_YDS.text());
+			 				json.PASS_TD = parseInt(PASS_TD.text());
+			 				json.PASS_INT = parseInt(PASS_INT.text());
+			 				json.RUSH = parseInt(RUSH.text());
+			 				json.RUSH_YDS = parseInt(RUSH_YDS.text());
+			 				json.RUSH_TD = parseInt(RUSH_TD.text());
+			 				json.REC = parseInt(REC.text());
+			 				json.REC_YDS = parseInt(REC_YDS.text());
+			 				json.REC_TD = parseInt(REC_TD.text());
+			 				json.REC_TAR = parseInt(REC_TAR.text());
+			 				json.MISC_FUML = parseInt(MISC_FUML.text());
+			 				json.MISC_TD = parseInt(MISC_TD.text());
+			 				json.PTS = parseFloat(PTS.text());
+
+			 				db.collection("owner" + owner_number + "_" + sport + "_stats_" + year).insert(json)
+
+			 				if (json.player == ""){
+			 					complete();
+			 				}
+
+						})
+
+
+					} // end of if(!error)
+				
+				}) // end of request
+
+		})
+
+			
+
+		var complete = function() {
+			complete_count += 1;
+
+			if (complete_count == owner_number_list.length){
+				callback();
+			}	
+		}
+
+	}) // end of forEach loop
+}
+
--- app/routes.js
+++ app/routes.js
@@ -7,6 +7,7 @@ var pyshell = require('python-shell');
 
 var mongo = require('mongodb');
 var assert = require('assert');
+var math = require('mathjs');
 
 // create router object
 var router = express.Router();
@@ -46,6 +47,78 @@ router.get('/', function(req, res) {
 	res.render('index', {
 		message: "Welcome to The Chip and Markers Trifecta Fantasy League Home Page"
 	});
+});
+
+router.get('/:sport/trades/:year', function(req, res) {
+
+	// set variables from request url
+	var sport = req.params.sport;
+	var year = req.params.year;
+
+	var trade = require('./trade.js')(req, res, db, sport, year, function(err, owner_number_list, trades_processed) {
+
+		owner_number_list = owner_number_list.filter(function(item, index, inputArray) {
+			return inputArray.indexOf(item) == index;
+		})
+		//console.log("new owner numbers: ", owner_number_list);
+
+		console.log("trades processed: ", trades_processed)
+
+
+		if (sport === 'basketball') {
+			var trade_stats = require('./basketball_trade_stats.js')(req, res, db, sport, year, owner_number_list, function(err, call) {
+				console.log("scrape done");
+				var options = {
+					args: [sport, year]
+				}
+
+				pyshell.run('basketball_trade_analysis.py', options, function(err) {
+					if (err) throw err;
+					console.log('trade python script complete');
+
+					db.collection(sport + "_trades_" + year).find({}, {"_id": 0}, {"sort": [["trade_number", "asc"], ["GP", "asc"], ["owner", "asc"]]}).toArray(function(e, docs) {
+						console.log('displaying trade analysis...');
+						//console.log(docs);
+						disp_trade = docs;
+						res.render('basketball_trade', {
+							year: year,
+							trader: disp_trade
+						})
+					})
+
+				})
+
+			}) 			
+		}
+
+		else if (sport === 'football') {
+			var trade_stats = require('./football_trade_stats.js')(req, res, db, sport, year, owner_number_list, function(err, call) {
+				console.log("scrape done");
+
+				var options = {
+					args: [sport, year]
+				}
+
+				pyshell.run('football_trade_analysis.py', options, function(err) {
+					if (err) throw err;
+					console.log('trade python script complete');
+
+					db.collection(sport + "_trades_" + year).find({}, {"_id": 0}, {"sort": [["trade_number", "asc"], ["owner", "asc"]]}).toArray(function(e, docs) {
+						console.log('displaying trade analysis...');
+						disp_trade = docs;
+						res.render('football_trade', {
+							year: year,
+							trader: disp_trade,
+							trades_processed: math.range(1, trades_processed)
+						})
+					})
+				})
+			}) 					
+		}
+
+	});
+
+
 });
 
 
@@ -89,7 +162,7 @@ router.get('/trifecta_standings/:year1/:year2', function(req, res) {
 				
 				// pull from this seasons trifecta collection
 				db.collection('trifecta_' + year1 + '_' + year2).find({}, {"_id": 0}).sort({"total_trifecta_points": -1}).toArray(function(e, docs){
-					//console.log(docs);
+					console.log(docs);
 					console.log("Displaying trifecta season standings...");
 					disp_trifecta_standings = docs;
 
