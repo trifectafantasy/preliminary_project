0f901941a84632b6b1ac2fc0978af3bba9c192d6
wesleychipeco <wescollege36@gmail.com>
2017-10-26 11:01:15 -0700

    Restructure coach and popular modules

---
+++ app/routes/coach.js
@@ -0,0 +1,99 @@
+///// IMPORT JAVASCRIPT PACKAGES //////
+let express = require('express');
+let request = require('request');
+let cheerio = require('cheerio');
+let path = require('path');
+let pyshell = require('python-shell');
+
+let mongo = require('mongodb');
+let assert = require('assert');
+
+function coach(req, res, db, args) {
+
+	let year = args.year;
+	let completed_football_season = args.completed_football_season;
+	let completed_weeks = args.completed_weeks;
+	let football_ahead = args.football_ahead;
+	let football_ahead_completed_matchups = args.football_ahead_completed_matchups;
+	
+// function that executes synchronous scrapes and python script analyses
+var all_football_coach = function(x, owner_list) {
+	if (x < owner_list.length) {
+		// set owner number
+		owner_number = owner_list[x];
+		console.log("owner number", owner_number);
+
+		// scrape starting lineups and benches from each week
+		var coach_scrape = require('../modules/coach_scrape.js')(req, res, db, year, owner_number, completed_weeks, function(err, call) {
+			console.log("coach scrape done");
+
+			// send back to loop again
+			all_football_coach(x + 1, owner_list);
+		}) // end of coach scrape script
+	}
+	// once done with all in loop, done
+	else {
+		var options = {
+			args: [year, completed_weeks]
+		}
+		pyshell.run('football_coach.py', options, function(err) {
+			if (err) throw err;
+			console.log("coach python script done");
+
+			// delay 2 seconds to let database populate
+			setTimeout(function() {
+				// pull from collection for display
+				db.collection("all_coach_" + year).find({}, {"_id": 0}).sort({"season": -1}).toArray(function(e, docs) {
+					disp_coach = docs;
+					console.log("displaying coach standings...");
+
+					res.render('football_coach', {
+						year: year,
+						completed_weeks: completed_weeks,
+						coach_standings: disp_coach
+					})
+				}) // end of display pull				
+			}, 2000)
+		}) // end of python script
+	}
+} // end of all_football_coach function
+
+// execute script
+	// if year is greater than completed season, SCRAPE
+	if (year > completed_football_season) {
+		
+		db.collection("all_coach_" + year).remove({});
+	
+		// how many weeks have been completed, able to scrape
+		if (football_ahead == true) {
+			completed_weeks = football_ahead_completed_matchups;
+		}
+
+		// list of owner numbers to loop through
+		owner_list = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+
+
+		// run synchronous for loop function
+		all_football_coach(0, owner_list);
+	} // end of if need to be scraped
+	// if year is in past, just pull
+	else {
+		completed_weeks = 13
+		// pull from collection for display
+		db.collection("all_coach_" + year).find({}, {"_id": 0}).sort({"season": -1}).toArray(function(e, docs) {
+			disp_coach = docs;
+			console.log("displaying coach standings...");
+
+			res.render('football_coach', {
+				year: year,
+				completed_weeks: completed_weeks,
+				coach_standings: disp_coach
+			})
+		}) // end of display pull	
+	} // end of just pull
+
+} // end of coach module
+
+module.exports = {
+	coach
+}
---
+++ app/routes/popular.js
@@ -0,0 +1,110 @@
+///// IMPORT JAVASCRIPT PACKAGES //////
+let express = require('express');
+let request = require('request');
+let cheerio = require('cheerio');
+let path = require('path');
+let pyshell = require('python-shell');
+
+let mongo = require('mongodb');
+let assert = require('assert');
+
+function popular(req, res, db, args) {
+
+	let year = args.year;
+	let sport = args.sport;
+	let completed_sport_season = args.completed_sport_season;
+	let score_cat = args.score_cat;
+
+// function that scrapes and calculates most popular players per owner per sport
+var all_popular = function(x, owner_list) {
+
+	if (x < owner_list.length) {
+		owner_number = owner_list[x];
+		console.log("owner number", owner_number);
+
+		var popular = require('../modules/popular_scrape.js')(req, res, db, sport, year, owner_number, function(err, call) {
+			console.log("popular scrape done");
+
+			var options = {
+				args: [sport, year, owner_number]
+			}
+
+			// python script that calculates players with most transactions per team
+			pyshell.run('popular_individual.py', options, function(err) {
+				console.log("popular python script done");
+
+				// send back through loop
+				all_popular(x + 1, owner_list)
+			}) // end of pyshell
+		}) // end of popular.js scrape
+	} // end of if still in loop
+	
+	// if finished with loop
+	else {
+		var options = {
+			args: [sport, year]
+		}
+
+		// python script that calculates player that was on most teams and owners who had player on roster
+		pyshell.run('popular_all.py', options, function(err) {
+			console.log("all popular python script done");
+
+			// delay 2 seconds to let database populate
+			setTimeout(function() {
+
+				// pull all owner names NOT "all", ie all individual owners
+				db.collection(sport + "_popular_" + year).find({"owner": {"$not": /all/}}, {"_id": 0}, {"sort": [["transactions", "desc"], ["owner", "desc"], [score_cat, "desc"]]}).toArray(function(e, docs) {
+					disp_individual = docs;
+
+					// pull owner that is all (has player on most teams data)
+					db.collection(sport + "_popular_" + year).find({"owner": "all"}, {"_id": 0}).toArray(function(e, docs2) {
+						disp_all = docs2;
+
+						console.log("displaying most popular players...")
+						res.render('popular', {
+							sport: sport,
+							year: year,
+							popular_individual: disp_individual,
+							popular_all: disp_all
+						}) // end of render
+					}) // end of all popular pull
+				}) // end of individual popular pull
+			}, 2000)
+		}) // end of pyshell
+	} // end of else
+} // end of function
+
+// start of executed script //
+	var owner_list = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+
+	// if season not completed, scrape
+	if (year > completed_sport_season) {
+
+		db.collection(sport + "_popular_" + year).remove({});
+
+		all_popular(0, owner_list);
+	}
+	// else just pull and display
+	else {
+		db.collection(sport + "_popular_" + year).find({"owner": {"$not": /all/}}, {"_id": 0}, {"sort": [["transactions", "desc"], ["owner", "desc"], [score_cat, "desc"]]}).toArray(function(e, docs) {
+			disp_individual = docs;
+
+			db.collection(sport + "_popular_" + year).find({"owner": "all"}, {"_id": 0}).toArray(function(e, docs2) {
+				disp_all = docs2;
+
+				console.log("displaying most popular players...")
+				res.render('popular', {
+					sport: sport,
+					year: year,
+					popular_individual: disp_individual,
+					popular_all: disp_all
+				}) // end od render
+			}) // end of all pull
+		}) // end of individual pull
+	} // end of else	
+
+} // end of popular module
+
+module.exports = {
+	popular
+}
